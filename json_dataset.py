6c40c2b7206eafcf49e178468b528f9a24884ab7
kingpinguin5 <utku.arslan@hotmail.com>
2021-11-14 14:59:15 +0100

    path update, commented json unpacking,
    *use dataset/spotify_million_playlist_dataset/src/* files to access the dataset as an interface

--- json_dataset.py
+++ json_dataset.py
@@ -1,20 +1,22 @@
-import csv
-import json
-import pandas as pd
-from Tools.scripts.dutree import display
-from pandas import json_normalize
-
-path = 'dataset/'
-
-with open(path + "/" + 'challenge_set.json', "r") as f:
-    data = json.load(f)
-
-df = pd.DataFrame(data)
-df = json_normalize(data, record_path='playlists', record_prefix='playlist_', errors='ignore')
-df.reset_index()
-print(df.head)
-print(df.count())
-print(df.columns)
-
-print(df["playlist_tracks"].loc[1499][0])  # access df[playlist_tracks].loc[df_index][track_index]
-# Print 1st track of 1500th playlist which is Beyonce 7/11
+"""Unused, use files under src to access dataset.
+    See preprocessing.py for usage example ....         """
+# import csv
+# import json
+# import pandas as pd
+# from pandas import json_normalize
+#
+#
+# path = 'dataset/spotify_million_playlist_dataset/subset'
+# with open(path + "/" + 'mpd.slice.0-999.json', "r") as f:
+#     data = json.load(f)
+#
+# print(data)
+# df = pd.DataFrame(data)
+# df = json_normalize(data, record_path='playlists', record_prefix='playlist_', errors='ignore')
+# df.reset_index()
+# print(df.head)
+# print(df.count())
+# print(df.columns)
+#
+# print(df["playlist_tracks"].loc[1499][0])  # access df[playlist_tracks].loc[df_index][track_index]
+# # Print 1st track of 1500th playlist which is Beyonce 7/11
--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,15 @@
-
 import matplotlib.pyplot as plt
 from dataset.spotify_million_playlist_dataset.src import *
 from dataset.spotify_million_playlist_dataset.src import stats
 
-[title, artist, track, length, followers] = stats.process_mpd(
-    "D:/PycharmProjects/the_recommenders/dataset/spotify_million_playlist_dataset/subset")
-platlist_length_histogram = sorted(length.items())
-x, y = zip(*platlist_length_histogram)
+path = "dataset/spotify_million_playlist_dataset/subset"  # path goes here
+[title, artist, track, length, followers] = stats.process_mpd(path)
+playlist_length_histogram = sorted(length.items())
+x, y = zip(*playlist_length_histogram)
 plt.plot(x, y, 'k')
 plt.xlabel("Number of tracks")
 plt.ylabel("Playlists")
-plt.text(60, .025, r'$\mu=%s$' % (stats.total_tracks/stats.total_playlists))
+plt.text(60, .025, r'$\mu=%s$' % (stats.total_tracks / stats.total_playlists))
 plt.grid(True)
 plt.title("Playlist length histogram")
 plt.show()
