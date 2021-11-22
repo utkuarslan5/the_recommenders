dc276fcbcb24ea8ce88badc2219307dc49372398
stelatopalova <56160328+stelatopalova@users.noreply.github.com>
2021-11-22 13:44:15 +0100

    dataframe to csv
    A new csv file with the playlist name, song name, artist name and album name
    
    Co-Authored-By: Joan Botzev <jbotzev@gmail.com>

--- dataset/spotify_million_playlist_dataset/subset/JSONtoDF.py
+++ dataset/spotify_million_playlist_dataset/subset/JSONtoDF.py
@@ -4,15 +4,21 @@ import json
 
 data = json.load(open('mpd.slice.0-999.json'))
 
-lat,lng,el = [],[],[]
+#json to dataframe ---------------------------------------------------------------------------------
+lat,lng,el, album = [],[],[], []
 for result in data['playlists']:
     for track_result in result['tracks']:
         lng.append(track_result[u'track_name'])
         el.append(track_result[u'artist_name'])
+        album.append(track_result[u'album_name'])
+
         lat.append(result[u'name'])
 
-df = pd.DataFrame([lat,lng, el]).T
-df.columns = ['playlist', 'track_name', 'artist_name']
+df = pd.DataFrame([lat,lng, el, album]).T
+df.columns = ['playlist', 'track_name', 'artist_name', 'album_name']
 pd.set_option('display.max_columns', None)
 final_df = df.sort_values(by=['playlist', 'artist_name'])
 print(final_df.head(100))
+
+# dataframe to csv ---------------------------------------------------------------------------------
+final_df.to_csv('songs.csv')
