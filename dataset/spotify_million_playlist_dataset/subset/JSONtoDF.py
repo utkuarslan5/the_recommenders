ca791e55c4405f1bef482d71e942c0f7d2db94ab
stelatopalova <56160328+stelatopalova@users.noreply.github.com>
2021-11-22 13:52:03 +0100

    All songs from all json files to the csv
    Co-Authored-By: Joan Botzev <jbotzev@gmail.com>

--- dataset/spotify_million_playlist_dataset/subset/JSONtoDF.py
+++ dataset/spotify_million_playlist_dataset/subset/JSONtoDF.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import pandas.io.json
 import json
-
+files = ['mpd.slice.0-999.json', 'mpd.slice.1000-1999.json', 'mpd.slice.2000-2999.json', 'mpd.slice.3000-3999.json', 'mpd.slice.4000-4999.json']
 data = json.load(open('mpd.slice.0-999.json'))
 
 #json to dataframe ---------------------------------------------------------------------------------
 lat,lng,el, album = [],[],[], []
-for result in data['playlists']:
-    for track_result in result['tracks']:
-        lng.append(track_result[u'track_name'])
-        el.append(track_result[u'artist_name'])
-        album.append(track_result[u'album_name'])
+for i in range(len(files)):
+    data = json.load(open(files[i]))
+    for result in data['playlists']:
+        for track_result in result['tracks']:
+            lng.append(track_result[u'track_name'])
+            el.append(track_result[u'artist_name'])
+            album.append(track_result[u'album_name'])
 
-        lat.append(result[u'name'])
+            lat.append(result[u'name'])
 
 df = pd.DataFrame([lat,lng, el, album]).T
 df.columns = ['playlist', 'track_name', 'artist_name', 'album_name']
