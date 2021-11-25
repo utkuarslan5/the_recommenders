1fad86664f7ec14f33dae184bd1ab09a3bb72e5a
Joan Botzev <jbotzev@gmail.com>
2021-11-25 15:25:41 +0100

    artist picking from NN fix

--- dataset/spotify_million_playlist_dataset/subset/KNN.py
+++ dataset/spotify_million_playlist_dataset/subset/KNN.py
@@ -1,71 +1,83 @@
 from sklearn.neighbors import NearestNeighbors
 import pandas as pd
-
+import math
 
 class KNN:
     pass
     """ KNN """
-    def __init__(self):
+
+    def __init__(self, num_nn, num_rec):
         #self.df = df #song
+        self.num_rec = num_rec
+        self.num_nn = num_nn
         self.df_csv = pd.read_csv('ratings.csv')
         self.rating_df = self.df_csv.pivot(index='user', columns='artist_name', values='rating')
         self.rating_df = self.rating_df.fillna(0)
         # print(song_data.head())
         # print(song_data['track_name'].value_counts())
 
-    def train(self):
-        knn = NearestNeighbors(n_neighbors=5, metric='cosine')
-        Model = knn.fit(self.rating_df)
+    '''
+        Recommend artist based on the most common artists between the neighbours
+        Sum the ratings for each artist from all nearest neighbours
+    '''
+    def recommend_artists(self):
+        knn = NearestNeighbors(n_neighbors=self.num_nn, metric='cosine')
+        model = knn.fit(self.rating_df)
         artists = self.rating_df.iloc[1,]
 
         # The indices contain only the index values of users from the user table.
-        distances, indices = Model.kneighbors([artists])
-        print("distances: " , distances)
-        print("indices (users close to our user): ", indices)
-        print("type:", type(indices))
+        distances, indices = model.kneighbors([artists])
+        global neighbors
         neighbors = indices[0][1:].tolist() # the users that are closest to the user we make a recommendation for
-        print("neighbours: ", neighbors)
 
-        neighbour_df = pd.DataFrame(columns=['user', 'artist_name', 'rating'])
-        neighbour_df = self.df_csv[self.df_csv['user'].isin(neighbors)]
-        pd.set_option('display.max_rows', 429)
+        neighbour_df = self.df_csv.loc[self.df_csv['user'].isin(neighbors)]
 
-        # print("neighbour dffffffffffffffffffffffffffffffffffff")
-        #print(neighbour_df)
-        #
-        new_df = neighbour_df.groupby(['user','artist_name'], as_index=False)['rating'].sum()
-        new_df2 = new_df.sort_values(by=['rating'], ascending=False)
-        print(new_df2[0:10])
+        pd.set_option('display.max_rows', None)
+        print(neighbors)
+        print(neighbour_df)
 
+        rec_artists_df = neighbour_df.groupby(['artist_name'], as_index=False)['rating'].sum()
+        rec_artists_df = rec_artists_df.sort_values(by=['rating'], ascending=False)
+        rec_artists_df = rec_artists_df[0:self.num_rec]
 
-        Recommended_Song = new_df2['artist_name'][0:10]
-        print('recommended songs: ')
-        print(Recommended_Song)
+        #if we want just artist names
+        #rec_artists_df = rec_artists_df['artist_name'][0:self.num_rec]
 
-        # neighbor_songs = pd.DataFrame(columns=['user', 'artist_name', 'rating'])
-        # for item in neighbors:
-        #     neighbor_songs = neighbor_songs.append(self.df_csv[self.df_csv.user == item], ignore_index=True)
-        # print("neighbour songs:")
-        # print(neighbor_songs)
-        #
-        # # for each artist find the total ratings of all neighbours
-        # neighbor_songs = pd.DataFrame({'rating': neighbor_songs['rating'], 'artist_name': neighbor_songs['artist_name']})
-        # neighbor_songs = neighbor_songs.groupby(['artist_name']).sum()
-        # neighbor_songs = neighbor_songs.sort_values(by=['rating'], ascending=False)
-        #
-        # print("song listen counts1:")
-        # print(neighbor_songs)
-        #
-        # neighbor_songs = neighbor_songs.sort_values('rating', ascending=False)
-        # print("song listen counts2:")
-        # print(neighbor_songs)
-        #
-        # neighbor_songs = neighbor_songs.reset_index(drop=True)
-        # print("neighbor songs")
-        # print(neighbor_songs)
-        #
-        # Recommended_Song = neighbor_songs['artist_name'][0:10]
-        # print('recommended songs: ')
-        # print(Recommended_Song)
+        print('\n recommended artists:')
+        print(rec_artists_df)
+
+        return rec_artists_df
+
+    '''
+        Recommending songs from the nearest neighbours' recommended artists
+        Song recommendation heuristics:
+            1) songs to recommend = n
+            2) songs per artist = ceil((rating(artist)/sum(ratings)) x n)
+    '''
+    def recommend_songs(self, df_ra, num_songs):
+        df_artists = df_ra
+        df_songs = pd.read_csv('users.csv')
+        df_songs = df_songs.loc[df_songs['user'].isin(neighbors)]
+
+        songs_artist = list(range(0,self.num_rec))
+        for i in range(len(df_artists)):
+            songs_artist[i] = math.trunc(round((df_artists['rating'].iloc[i]/df_artists['rating'].sum())*num_songs))
+
+        print("\nsongs per artist: ", songs_artist)
+        print("neighbours list: ", neighbors)
+
+        recommended_songs = list()
+        k = 0
+        for i in songs_artist:
+            print("\n----- Amount of songs from  ----- ",df_artists['artist_name'].iloc[k], " ---- ",i)
+            for user_index in range(i):
+                song = df_songs.loc[df_songs['artist_name'] == df_artists['artist_name'].iloc[k], 'track_name'].iloc[user_index]
+                recommended_songs.append(song)
+                print("song number: ", user_index, " is ", song)
+            k += 1
+
+        print("\n amount of artists:",k)
+        print("\n Recommended songs:")
+        print(recommended_songs)
 
 
--- dataset/spotify_million_playlist_dataset/subset/main.py
+++ dataset/spotify_million_playlist_dataset/subset/main.py
@@ -5,7 +5,7 @@ def main():
 
     print("Generating users . . .")
 
-    # num_users = 11
+    num_users = 11
     # num_PL_perUser = 2
     # users = Users()
     # users.generate_users(num_users,num_PL_perUser)
@@ -16,10 +16,14 @@ def main():
     # print("Generating ratings . . . ")
     # rating_df = users.ratings_by_artists(df)
 
-    print("train KNN . . .")
+    print("Loading recommendations KNN . . .")
 
-    knn = KNN()
-    knn.train()
+    art_rec = 10
+    song_rec = 50
+    neighbours = 5
+    knn = KNN(neighbours, art_rec)
+    artists = knn.recommend_artists()
+    songs = knn.recommend_songs(artists, song_rec)
 
 if __name__ == "__main__":
    main()
